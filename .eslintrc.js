6bdead403af5ef24e533b957b3d1964d99c0a8ab
AlanLee <1445654576@qq.com>
2023-09-01 15:56:26 +0800

    fix: 使用包的规则

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,25 +1,16 @@
 module.exports = {
-  env: {
-    browser: true,
-    commonjs: true,
-    es2021: true,
-    node: true,
-  },
-  extends: 'airbnb-base',
-  parserOptions: {
-    ecmaVersion: 'latest',
-  },
-  ignorePatterns: ['dist/*.js'],
-  rules: {
-    // 0 - off, 1 - warn, 2 - error
-    'import/extensions': 1,
-    'no-param-reassign': 1,
-    'import/prefer-default-export': 0,
-    'global-require': 1,
-    'default-param-last': 1,
-    'prefer-const': 1,
-    'max-len': 0,
-    'import/no-extraneous-dependencies': 0,
-    'linebreak-style': 0,
-  },
+  // env: {
+  //   browser: true,
+  //   commonjs: true,
+  //   es2021: true,
+  //   node: true,
+  // },
+  extends: 'alanlee',
+  // parserOptions: {
+  //   ecmaVersion: 'latest',
+  // },
+  // ignorePatterns: ['dist/*.js'],
+  // rules: {
+  //   // 0 - off, 1 - warn, 2 - error
+  // },
 };
