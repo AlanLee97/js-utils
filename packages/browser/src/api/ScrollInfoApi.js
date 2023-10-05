e8cd10e5351305f983e442673384907e23c8dfa1
AlanLee <1445654576@qq.com>
2023-10-05 15:34:02 +0800

    feat: 增加函数toKebabString、nextTick、ScrollInfo

---
+++ packages/browser/src/api/ScrollInfoApi.js
@@ -0,0 +1,11 @@
+function noop() {}
+
+export class ScrollInfoApi {
+  onScroll = noop;
+
+  scrollToTop = noop;
+
+  scrollToBottom = noop;
+
+  destroy = noop;
+}
--- packages/browser/src/utils/browser.js
+++ packages/browser/src/utils/browser.js
@@ -1,3 +1,5 @@
+import { ScrollInfoApi } from '../api/ScrollInfoApi';
+
 /**
  * a标签下载文件（可能存在跨域问题）
  * @env browser
@@ -63,4 +65,100 @@ export function className(...args) {
   });
 
   return res.trim();
+}
+
+/**
+ * 转成烤串字符串
+ * @param {*} string
+ */
+export function toKebabString(str = '') {
+  const testA2Z = (s = '') => /[A-Z]/.test(s);
+  let res = '';
+  str.split('').forEach((s, i) => {
+    if (testA2Z(s)) {
+      // eslint-disable-next-line no-nested-ternary
+      res += `${i === 0 ? '' : str[i - 1] === '-' ? '' : '-'}${s.toLowerCase()}`;
+    } else {
+      res += s;
+    }
+  });
+  return res;
+}
+
+/**
+ * 下一个微任务的循环
+ * @param {*} cb
+ * @returns
+ */
+export function nextTick(fn = () => {}) {
+  return Promise.resolve().then(() => {
+    if (typeof fn === 'function') {
+      fn();
+    }
+  });
+}
+
+/**
+ * 滚动相关信息
+ */
+export class ScrollInfo extends ScrollInfoApi {
+  constructor(options = {}) {
+    super();
+    this.target = options.target || window;
+    this.scrollCallback = options.onScroll || this.scrollCallback;
+    this.scrollX = 0;
+    this.scrollY = 0;
+    this.scrollHeight = 0;
+    this.scrollPercent = 0;
+    this.atTop = true;
+    this.atBottom = false;
+    this.isValidTarget = this.target && (this.target instanceof HTMLElement || this.target === window);
+    this.instance = this;
+
+    this.init();
+  }
+
+  init() {
+    this.target.addEventListener('scroll', this.onScroll);
+  }
+
+  // eslint-disable-next-line class-methods-use-this
+  scrollCallback = () => {};
+
+  onScroll = (e) => {
+    const { scrollTop, scrollHeight } = e.target || {};
+    this.scrollHeight = scrollHeight;
+    this.scrollY = scrollTop;
+    const original = scrollTop / scrollHeight;
+    // eslint-disable-next-line no-mixed-operators
+    this.scrollPercent = Math.floor((original - 0) / (0.54 - 0) * 100);
+    this.atTop = scrollTop === 0;
+    this.atBottom = Math.floor(this.target.clientHeight + scrollTop) === scrollHeight;
+    this.event = e;
+    this.scrollCallback(this);
+  };
+
+  scrollToTop = () => {
+    if (this.isValidTarget) {
+      this.target.scrollTo({
+        top: 0,
+        left: 0,
+        behavior: 'smooth',
+      });
+    }
+  };
+
+  scrollToBottom = () => {
+    if (this.isValidTarget) {
+      this.target.scrollTo({
+        top: this.scrollHeight,
+        left: 0,
+        behavior: 'smooth',
+      });
+    }
+  };
+
+  destroy = () => {
+    this.target.removeEventListener('scroll', this.onScroll);
+  };
 }
